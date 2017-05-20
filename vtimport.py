747b79b63fdb5250cc5aa8f77bf05d53883773f6
Tyler Carr <tcarr@TMC-Little-MacBook-Air.local>
2017-05-19 22:12:55 -0400

    Refactored buyerlist and sellerlist

--- vtimport.py
+++ vtimport.py
@@ -12,13 +12,39 @@ class VtMongoParser:
 		self.coll = self.cl["proptrans"]["collectionname"]
 		self.coll.remove()
 
-	def xmlToDict(self, parentDict, xmlElement):
+	#def xmlToDict(self, parentDict, xmlElement):
+	#	if len(xmlElement.getchildren()) > 0:
+	#		if xmlElement.tag == "sellerList" or xmlElement.tag == "buyerList":
+	#			parentDict[xmlElement.tag]=[]
+	#			for c in xmlElement.getchildren():
+	#				self.xmlToDict(parentDict[xmlElement.tag], c)
+	#		else:
+	#			parentDict[xmlElement.tag]={}
+	#			for c in xmlElement.getchildren():
+	#				self.xmlToDict(parentDict[xmlElement.tag], c)
+	#	else:
+	#		parentDict[xmlElement.tag]=xmlElement.text
+	def xmlToDict(self, xmlElement):
+		elementDict={}
 		if len(xmlElement.getchildren()) > 0:
-			parentDict[xmlElement.tag]={}
-			for c in xmlElement.getchildren():
-				self.xmlToDict(parentDict[xmlElement.tag], c)
+			#if xmlElement.tag == "sellerList" or xmlElement.tag == "buyerList":
+			#	elementDict[xmlElement.tag]=[]
+			#	for c in xmlElement.getchildren():
+			#		listDict=self.xmlToDict(c)
+			#		elementDict[xmlElement.tag].append(listDict)
+			#else:
+				elementDict[xmlElement.tag]={}
+				for c in xmlElement.getchildren():
+					if c.tag == "sellerList" or c.tag == "buyerList":
+						elementDict[xmlElement.tag][c.tag]=[]
+						for t in c.getchildren():
+							listDict=self.xmlToDict(t)
+							elementDict[xmlElement.tag][c.tag].append(listDict)
+					else:	
+						elementDict[xmlElement.tag][c.tag]=self.xmlToDict(c)
 		else:
-			parentDict[xmlElement.tag]=xmlElement.text
+			return xmlElement.text
+		return elementDict
 
 	def loadMongo(self, filepath):
 		print "Digesting XML file: "+str(filepath)
@@ -26,18 +52,17 @@ class VtMongoParser:
 		i = 0
 		for event in xml.getroot().xpath("//formData"):
 			i+=1
-			proptransrecord = {}
-			self.xmlToDict(proptransrecord,event)
+			proptransrecord = self.xmlToDict(event)
 			#print "inserting record #"+str(i)
 			#self.coll.insert(proptransrecord)
 			self.coll.insert(proptransrecord)
 
 
 parser = VtMongoParser()
-for filename in glob.glob("/Users/tcarr/proptrans/PT2014*.xml"):
+for filename in glob.glob("/Users/tcarr/proptrans/PT201401*.xml"):
 	parser.loadMongo(filename)
 
-results=parser.coll.find({"formData.sellerList.seller.lastName": "SMITH"})
+results=parser.coll.find({"formData.buyerList.buyer.lastName": "HAYES"})
 #results=parser.coll.collectionname.find({"lastName": "SMITH"})
 print results.count()
 for item in results:
