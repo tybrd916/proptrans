1e4974cd7d98294cc6b92fa5471cc0c67561415c
Tyler Carr <tcarr@TMC-Little-MacBook-Air.local>
2017-05-19 22:15:01 -0400

    removing old commented code

--- vtimport.py
+++ vtimport.py
@@ -12,36 +12,18 @@ class VtMongoParser:
 		self.coll = self.cl["proptrans"]["collectionname"]
 		self.coll.remove()
 
-	#def xmlToDict(self, parentDict, xmlElement):
-	#	if len(xmlElement.getchildren()) > 0:
-	#		if xmlElement.tag == "sellerList" or xmlElement.tag == "buyerList":
-	#			parentDict[xmlElement.tag]=[]
-	#			for c in xmlElement.getchildren():
-	#				self.xmlToDict(parentDict[xmlElement.tag], c)
-	#		else:
-	#			parentDict[xmlElement.tag]={}
-	#			for c in xmlElement.getchildren():
-	#				self.xmlToDict(parentDict[xmlElement.tag], c)
-	#	else:
-	#		parentDict[xmlElement.tag]=xmlElement.text
 	def xmlToDict(self, xmlElement):
 		elementDict={}
 		if len(xmlElement.getchildren()) > 0:
-			#if xmlElement.tag == "sellerList" or xmlElement.tag == "buyerList":
-			#	elementDict[xmlElement.tag]=[]
-			#	for c in xmlElement.getchildren():
-			#		listDict=self.xmlToDict(c)
-			#		elementDict[xmlElement.tag].append(listDict)
-			#else:
-				elementDict[xmlElement.tag]={}
-				for c in xmlElement.getchildren():
-					if c.tag == "sellerList" or c.tag == "buyerList":
-						elementDict[xmlElement.tag][c.tag]=[]
-						for t in c.getchildren():
-							listDict=self.xmlToDict(t)
-							elementDict[xmlElement.tag][c.tag].append(listDict)
-					else:	
-						elementDict[xmlElement.tag][c.tag]=self.xmlToDict(c)
+			elementDict[xmlElement.tag]={}
+			for c in xmlElement.getchildren():
+				if c.tag == "sellerList" or c.tag == "buyerList":
+					elementDict[xmlElement.tag][c.tag]=[]
+					for t in c.getchildren():
+						listDict=self.xmlToDict(t)
+						elementDict[xmlElement.tag][c.tag].append(listDict)
+				else:	
+					elementDict[xmlElement.tag][c.tag]=self.xmlToDict(c)
 		else:
 			return xmlElement.text
 		return elementDict
@@ -53,8 +35,6 @@ class VtMongoParser:
 		for event in xml.getroot().xpath("//formData"):
 			i+=1
 			proptransrecord = self.xmlToDict(event)
-			#print "inserting record #"+str(i)
-			#self.coll.insert(proptransrecord)
 			self.coll.insert(proptransrecord)
 
 
